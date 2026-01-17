0ebcd4fc050e6d82b41dc18ca4d9f887432f3441
Jorge Ramon <jorge.ramon2128@gmail.com>
2026-01-17 09:21:33 -0600

    Codigo base del API de Netflix

---
+++ sesion05-best-practices/src/test/java/org/bedu/netflix/NetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.netflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
